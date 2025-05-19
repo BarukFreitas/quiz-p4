755fa4a88d044818b833db0773f802b940c041bb
Baruk Freitas <baruk.freitas@gmail.com>
2025-05-19 09:29:24 -0300

    quiz funcionando corretamente

--- app/components/molecules/AnswerFeedback.tsx
+++ app/components/molecules/AnswerFeedback.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from 'react';
 import Image from '../atoms/Image';
 import Typography from '../atoms/Typography';
@@ -11,16 +13,15 @@ interface AnswerFeedbackProps {
   onNext: () => void;
 }
 
-const AnswerFeedback: React.FC<AnswerFeedbackProps> = ({ isCorrect, correctAnswer, characterName, fullImage, onNext }) => {
+const AnswerFeedback: React.FC<AnswerFeedbackProps> = ({ isCorrect, characterName, fullImage, onNext }) => {
   return (
-    <div className="bg-white rounded-lg shadow-md p-4 flex flex-col items-center">
+    <div className="bg-white rounded-lg shadow-md p-4 flex flex-col items-center text-center"> {/* Adicionado text-center */}
       {isCorrect ? (
-        <Typography variant="h2" className="text-green-500 mb-2">Correto!</Typography>
+        <Typography variant="h2" className="text-green-500 mb-2">Correto! <br /> {characterName}</Typography>
       ) : (
-        <Typography variant="h2" className="text-red-500 mb-2">Incorreto!</Typography>
+        <Typography variant="h2" className="text-red-500 mb-2">Incorreto! A resposta correta é: <br /> {characterName}</Typography>
       )}
       <Image src={fullImage} alt={characterName} width={150} height={150} className="mb-4 object-contain rounded-md" />
-      <Typography variant="p" className="mb-4 text-center">A resposta correta era: <span className="font-bold">{correctAnswer}</span> ({characterName})</Typography>
       <Button onClick={onNext} variant="primary">Próxima Pergunta</Button>
     </div>
   );
